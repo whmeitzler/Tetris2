f9a23aa0a7c3c4786e3742be457b763c454eca92
whmeitzler <whmeitzler@olivet.edu>
2015-01-29 17:12:50 -0600

    It totally works I just need to get rid of all the exceptions

---
+++ src/Game.java
@@ -0,0 +1,16 @@
+import java.awt.Rectangle;
+
+import javax.swing.JFrame;
+
+public class Game  {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JFrame f = new JFrame();
+		f.setMaximizedBounds(new Rectangle(20, 20));
+		System.out.println("Hello");
+		System.out.println("oops");
+		
+	}
+
+}
